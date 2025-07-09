5f69b3476be46118350cbb5f3b3b2145711581ec
Omotuka Busayo <47152329+busayogabriel47@users.noreply.github.com>
2025-07-09 22:34:48 +0200

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -1,56 +1,9 @@
-import asyncio
-import json
-import os
-import sys
-import time
+import logging
 import azure.functions as func
-from datetime import datetime, timedelta
-from dotenv import load_dotenv
-load_dotenv()  # This loads environment variables from a .env file in the current directory
-import random
-import string
-import subprocess
-import shutil
-import platform
-import webbrowser
-import dns.resolver
-from azure.core.exceptions import ClientAuthenticationError
-from azure.identity import ClientSecretCredential
-from azure.storage.blob import BlobServiceClient, generate_blob_sas, BlobSasPermissions
-
-from azure.mgmt.resource import ResourceManagementClient
-from azure.mgmt.network import NetworkManagementClient
-from azure.mgmt.network.models import NetworkSecurityGroup, SecurityRule, NetworkInterface
-from azure.mgmt.compute import ComputeManagementClient
-from azure.mgmt.compute.models import (
-    VirtualMachine, HardwareProfile, StorageProfile,
-    OSProfile, NetworkProfile, NetworkInterfaceReference,
-    VirtualMachineExtension, WindowsConfiguration,SecurityProfile
-)
-from azure.mgmt.dns import DnsManagementClient
-from azure.mgmt.dns.models import RecordSet
-from azure.mgmt.storage import StorageManagementClient
-
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
 @app.route(route="createvm")
 def createvm(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+    return func.HttpResponse("Test function executed successfully.", status_code=200)
